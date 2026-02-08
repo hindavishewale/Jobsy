38b61bbe06f9b595eeee2ff2144a9ab04971c4e2
Vaibhav Salve <144205049+abcdjjejejsj@users.noreply.github.com>
2026-02-08 09:49:27 +0530

    Add updateProfile route and getCandidateByEmail

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -2,17 +2,10 @@ const express = require("express");
 const handler = require("../Controllers/handler");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const { getUserRisk } = require("../Controllers/userController.js");
-
 const sec = process.env.secret_key;
-
-// ---------------------
-// Validate User Middleware
-// ---------------------
 function validateUser(req, res, next) {
   const token = req.cookies.token;
   if (!token) return res.redirect("/register");
-
   jwt.verify(token, sec, (err, user) => {
     if (err) {
       res.clearCookie("token", {
@@ -26,16 +19,13 @@ function validateUser(req, res, next) {
     next();
   });
 }
-
-// ---------------------
-// Routes
-// ---------------------
 router.post("/signup", handler.upload.single("Resume"), handler.addData);
 router.post("/signup/company",handler.addCompany);
 router.post("/signin", handler.loginUser);
 router.post("/internships",handler.addInternship);
 router.get("/getUser",validateUser,handler.loader);
-
+router.get("/getCandidateByEmail",handler.getCandidateByEmail);
+router.post("/updateProfile",validateUser,handler.updateProfile);
 router.get("/admin", validateUser, handler.admin);
 router.get("/user/:pge", validateUser, handler.userSpecific);
 router.get("/loadData/profile", validateUser, handler.loader);
@@ -46,5 +36,4 @@ router.get("/getContact", handler.getContact);
 router.post("/changeStatus", handler.changeStatus);
 router.post("/deletemesg", handler.deletemesg);
 router.get("/logout", handler.logout);
-
 module.exports = router;
