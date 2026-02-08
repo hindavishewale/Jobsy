6a3a7ce5992687a2c5a9ca8010112f82c0cab885
Vaibhav Salve <144205049+abcdjjejejsj@users.noreply.github.com>
2026-02-08 09:48:59 +0530

    Remove commented code and clean up routes.js

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -1,9 +1,5 @@
-// Routes/route.js
 const express = require("express");
 const { getMatchedInternships } = require("../Controllers/bert");
-
 const router = express.Router();
-
 router.post("/matchInternships", getMatchedInternships);
-
 module.exports = router;
