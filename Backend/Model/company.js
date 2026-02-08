c805f5a371e6b4bc8704b33773e7a97691471ae8
Vaibhav Salve <144205049+abcdjjejejsj@users.noreply.github.com>
2026-02-08 09:46:13 +0530

    Modify company model and MongoDB connection
    Updated MongoDB connection string and added new fields for verification status, registration date, and location.

--- Backend/Model/company.js
+++ Backend/Model/company.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/SIH")
+mongoose.connect("mongodb://127.0.0.1:27017/Jobsy")
   .then(() => {
     console.log("Mongo connected for company");
   })
   .catch((err) => console.log("error of mongo company : ", err));
-
 const companySchema = new mongoose.Schema({
   CompanyName: {
     type: String,
@@ -39,8 +37,18 @@ const companySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  verificationStatus: {
+    type: String,
+    default: 'Pending',
+  },
+  registrationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  Location: {
+    type: String,
+    default: 'Not specified',
+  },
 });
-
 const companyModel = mongoose.model("company", companySchema);
-
 module.exports = companyModel;
