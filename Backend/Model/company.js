6754d73b1ea75857eeb98dae722098d088f17dd2
Pratap Sanap <144205384+PRATAPSANAP1@users.noreply.github.com>
2026-02-19 17:36:24 +0530

    Remove MongoDB connection from company model
    Removed MongoDB connection logic from company model.

--- Backend/Model/company.js
+++ Backend/Model/company.js
@@ -1,9 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Mongo connected for company");
-  })
-  .catch((err) => console.log("error of mongo company : ", err));
 const companySchema = new mongoose.Schema({
   CompanyName: {
     type: String,
