9e48e4260653f01d46f41b4196f40e1910634739
Pratap Sanap <144205384+PRATAPSANAP1@users.noreply.github.com>
2026-02-19 16:07:31 +0530

    Update MongoDB connection URI to use environment variable

--- Backend/Model/notification.js
+++ Backend/Model/notification.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/Jobsy")
+mongoose.connect("process.env.MONGO_URI")
 .then(()=>console.log("Mongo connected for notifications"))
 .catch(err=>console.log("error of mongo notifications:",err));
 const notificationSchema=new mongoose.Schema({
