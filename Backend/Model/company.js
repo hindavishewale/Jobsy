056e6bd634a421d5589531f1141656224cfd2bdd
Pratap Sanap <144205384+PRATAPSANAP1@users.noreply.github.com>
2026-02-19 17:21:40 +0530

    Fix connection string syntax in company.js

--- Backend/Model/company.js
+++ Backend/Model/company.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("process.env.MONGO_URI")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Mongo connected for company");
   })
